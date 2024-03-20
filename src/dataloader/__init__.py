5d739b39b213e7e7e5aa26209a3c758f2cb99a50
Jin Hyoung Joo <hyoungjoo.j@gmail.com>
2024-03-20 09:27:26 +0900

    feat: add dataloader module __init__

---
+++ src/dataloader/__init__.py
@@ -0,0 +1,49 @@
+import importlib
+import os
+from typing import Dict
+
+from torch.utils.data import DataLoader
+
+from ..logger import Logger
+from ..registry import Registry
+from ..utils import import_modules
+
+# Scan all files in directory to add modules to registry.
+for module in import_modules(os.path.dirname(__file__)):
+    importlib.import_module(f"{__name__}.{module}")
+
+
+def build_dataloader(config: Dict) -> DataLoader:
+    """Build dataloader using configuration options.
+
+    Args:
+        config (Dict): The configuration options for the dataloader. The
+          dictionary must include a name field and an optional args field.
+          The name field value is used to search the corresponding ovject
+          from the registry.
+
+    Returns:
+        DataLoader: A dataloader object retrieved from the registry.
+
+    """
+    try:
+        name = config["name"]
+    except KeyError:
+        Logger.error("Missing name field in dataloader configuration!")
+        exit(-1)
+
+    parameters = config.get("args", {})
+
+    try:
+        dataloader = Registry.get(
+            prefix="data",
+            name=name,
+        )(**parameters)
+
+    except KeyError as e:
+        Logger.error(f"Error generating {name}\n{e}")
+        exit(-1)
+
+    Logger.info(f"Dataloader build success.\n\t{dataloader}")
+
+    return dataloader
